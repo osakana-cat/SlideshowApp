ec6928e1109e8dc5a8f0220ae2faefa43195d27e
さき <saki@saki-no-macbook-pro.local>
2015-12-08 00:01:47 +0900

    Initial Commit

---
+++ SlideshowApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SlideshowApp
+//
+//  Created by さき on 2015/12/08.
+//  Copyright © 2015年 saki.yokota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
