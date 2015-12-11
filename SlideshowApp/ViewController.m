9af23446b67ac9b2351fe11a7d22b8ca91f13ff9
さき <saki@saki-no-macbook-pro.local>
2015-12-11 23:15:22 +0900

    アップロードし直しました

--- SlideshowApp/ViewController.m
+++ SlideshowApp/ViewController.m
@@ -8,7 +8,12 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController (){
+    
+    UIImageView *aImageView;
+    NSInteger countNumber;
+    
+}
 
 @end
 
@@ -16,8 +21,84 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self setupBackground];
+    [self setupButton];
+    
 }
+
+
+-(void)setupBackground{
+    aImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 320, 568)];
+    aImageView.image = [UIImage imageNamed:@"flower1.png"];
+    [self.view addSubview:aImageView];
+}
+
+
+-(void)setupButton{
+    
+    //①戻るボタンを作成する
+    UIButton *Button1 = [UIButton buttonWithType:UIButtonTypeCustom];
+    
+    //サイズ・デザインとか
+    Button1.frame = CGRectMake(0, 0, 120, 45);
+    Button1.center = CGPointMake(160, 50);
+    
+    [Button1 setImage:[UIImage imageNamed:@"modoru_yajirusi.png"] forState:UIControlStateNormal];
+    
+    //押された時
+[Button1 addTarget:self action:@selector(changeImageUsingIf1:) forControlEvents:UIControlEventTouchUpInside];
+    
+    //画面に貼り付ける
+    [self.view addSubview:Button1];
+    
+    
+    
+    //②進むボタンを作成する
+    UIButton *Button2 = [UIButton buttonWithType:UIButtonTypeCustom];
+    
+    //サイズ・デザインとか
+    Button2.frame = CGRectMake(0, 0, 120, 45);
+    Button2.center = CGPointMake(160, 518);
+    
+   [Button2 setImage:[UIImage imageNamed:@"susumu_yajirusi.png"] forState:UIControlStateNormal];
+    
+    //押された時
+[Button2 addTarget:self action:@selector(changeImageUsingIf2:) forControlEvents:UIControlEventTouchUpInside];
+    
+    //画面に貼り付ける
+    [self.view addSubview:Button2];
+
+}
+
+
+//戻るボタンで呼び出されるメソッド  -(void)メソッド名:(id)sender
+-(void)changeImageUsingIf1:(id)sender{
+    countNumber--;
+    
+    
+    if (countNumber <= 0) {
+        
+        countNumber = 3;
+    }
+    aImageView.image = [UIImage imageNamed:[NSString stringWithFormat:@"flower%ld.png",countNumber]];
+}
+
+
+//進むボタンで呼び出されるメソッド  -(void)メソッド名:(id)sender
+-(void)changeImageUsingIf2:(id)sender{
+    countNumber++;
+    
+    if (countNumber >= 4){
+        
+        countNumber = 1;
+        
+    }
+    aImageView.image = [UIImage imageNamed:[NSString stringWithFormat:@"flower%ld.png",countNumber]];
+}
+
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
